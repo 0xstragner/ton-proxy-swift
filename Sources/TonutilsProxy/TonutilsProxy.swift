1126bf58038deea86323cdf799d9b2acc01c674e
Adam Stragner <adam@stragner.com>
2023-07-17 15:12:27 +0400

    Add NEPacketTunnelProvider default implementation

--- Sources/TonutilsProxy/TonutilsProxy.swift
+++ Sources/TonutilsProxy/TonutilsProxy.swift
@@ -5,6 +5,8 @@
 import Foundation
 import TonutilsProxyBridge
 
+// MARK: - TonutilsProxy
+
 public final class TonutilsProxy: TPBTunnel {
     public static var shared: TonutilsProxy {
         shared()
@@ -13,5 +15,16 @@ public final class TonutilsProxy: TPBTunnel {
     @discardableResult
     public func start(_ port: UInt16 = 9090) async throws -> TPBTunnelParameters {
         try await start(withPort: port)
+    }
+}
+
+// MARK: TonutilsProxy.SupportedDomain
+
+public extension TonutilsProxy {
+    enum SupportedDomain: String, CaseIterable, RawRepresentable {
+        case ton
+        case adnl
+        case tme = "t.me"
+        case bag
     }
 }
